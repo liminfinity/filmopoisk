2a6d5e4c6b1946f4297ff3243bc5fbad6f1322e5
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-06-29 10:53:54 +0700

    init project

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    port: 3002,
+    strictPort: true
+  },
+  server: {
+    port: 3001,
+    strictPort: true
+  },
+})
